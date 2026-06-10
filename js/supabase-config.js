e8892e48b3d47454344948a84d9e28207882814b
Alok-MPM <legendsigmamale@gmail.com>
2026-06-10 19:47:53 +0530

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,4 +1,7 @@
 const SUPA_URL = 'https://ucwgvvsnellchioltkxs.supabase.co';
 const SUPA_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVjd2d2dnNuZWxsY2hpb2x0a3hzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAwODEyOTYsImV4cCI6MjA5NTY1NzI5Nn0.goU5F94rJcih_Nv8Gqp0xR7LjAH8n3zqp8qtLYLQPZM';
 const { createClient } = supabase;
-const db = createClient(SUPA_URL, SUPA_KEY);
+const supabase = createClient(SUPA_URL, SUPA_KEY);
+const db = supabase;
+
+
